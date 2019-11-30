fca1fe723e9ff7eaad90eae911689a39e4c1f132
Tuna Ayberk Ozmen <tunaayberk@hotmail.com>
2019-11-30 00:23:47 -0500

    create-react-app updated with first homepage layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import HomePage from "./Home/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <HomePage />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
