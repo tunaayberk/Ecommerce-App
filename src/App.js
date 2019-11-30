1f474cec008448d218a1975285a949c3d6581671
Tuna Ayberk Ozmen <tunaayberk@hotmail.com>
2019-11-30 01:53:43 -0500

    menu component updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./Home/HomePage";
 import ShopPage from "./Shop/ShopPage";
+import Header from "./Shared/Navigation/Header";
 
 import "./assets/style/App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
